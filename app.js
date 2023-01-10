e6bbaeae735fc6b677983e2139dd4a08462279b3
bluff <bluffyx91@gmail.com>
2023-01-10 18:50:32 +0200

    added security with helmet, cors, xss and rate-limit

--- app.js
+++ app.js
@@ -5,9 +5,22 @@ const app = express()
 const router = require('./routes/router')
 const mongoose = require('mongoose')
 mongoose.set("strictQuery", false);
+const helmet = require('helmet')
+const cors =  require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
 
+const limiter = rateLimiter({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100
+})
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+app.use(limiter)
 app.use(express.json())
 app.use(router)
+
 
 
 
